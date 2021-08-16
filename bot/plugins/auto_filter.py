549215faab558b2db69b097eb6814e67fce7ba08
Hitenop <87300913+Hitenop@users.noreply.github.com>
2021-08-16 21:15:13 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -184,7 +184,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"**😃 Got it \n\nYour Query 👉 {query}**",
+                text=f"**😃 Got it \n\nYour @request_moviesorseries movie👉 {query}**",
                 reply_markup=reply_markup,
                 parse_mode="markdown",
                 reply_to_message_id=update.message_id
